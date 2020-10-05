49b320dcd81db5426ce289e743e756eae5ce8069
wwwaaa <python.signal@gmail.com>
2020-10-05 23:20:34 +0900

    feat: material-ui Drawer 이용해서 사이드바 구현

--- src/App.js
+++ src/App.js
@@ -1,38 +1,172 @@
-import React from "react";
-import Calendar from "./components/calendar";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Calendar from './components/calendar';
 
-const useStyles = makeStyles((theme) => ({
+const LeftSidebarWidth = 240;
+
+const useStyles = makeStyles(theme => ({
   root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${LeftSidebarWidth}px)`,
+    marginLeft: LeftSidebarWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: LeftSidebarWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: LeftSidebarWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -LeftSidebarWidth,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-  header: {
-    borderBottom: "1px solid #ccc",
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
 function App() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const [transitionEnded, setTransitionEnded] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
     <>
-      <div className={classes.header}>캘린더</div>
       <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item md={2}>
-            <Paper className={classes.paper}>내 캘린더</Paper>
-            <Paper className={classes.paper}>다른 캘린더</Paper>
-          </Grid>
-          <Grid item md>
-            <Calendar />
-          </Grid>
-        </Grid>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              캘린더
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <Button>만들기</Button>
+          <List>
+            {['내 캘린더'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['다른 캘린더'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <main
+          onTransitionEnd={() => setTransitionEnded(!transitionEnded)}
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <Calendar sidebarOpened={transitionEnded} />
+        </main>
       </div>
     </>
   );
--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,14 +1,18 @@
-import React from "react";
-import { useObserver } from "mobx-react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import luxonPlugin from "@fullcalendar/luxon";
-import rrulePlugin from "@fullcalendar/rrule";
+import React, { useEffect, useRef } from 'react';
+import { useObserver } from 'mobx-react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import luxonPlugin from '@fullcalendar/luxon';
+import rrulePlugin from '@fullcalendar/rrule';
 // import { getLunar } from "holiday-kr";
 
-const Calendar = () => {
+const Calendar = ({ sidebarOpened }) => {
+  const ref = useRef('ref');
+  useEffect(() => {
+    ref.current.getApi().updateSize();
+  }, [sidebarOpened]);
   function handleEventClick(clickInfo) {}
 
   function handleDateSelect(selectInfo) {
@@ -31,7 +35,8 @@ const Calendar = () => {
   return useObserver(() => (
     <>
       <FullCalendar
-        height="700px"
+        ref={ref}
+        height="auto"
         plugins={[
           dayGridPlugin,
           timeGridPlugin,
@@ -40,18 +45,18 @@ const Calendar = () => {
           rrulePlugin,
         ]}
         headerToolbar={{
-          start: "prev,next",
-          center: "title",
-          end: "today dayGridMonth,timeGridWeek,timeGridDay",
+          start: 'prev,next',
+          center: 'title',
+          end: 'today dayGridMonth,timeGridWeek,timeGridDay',
         }}
         locale="ko"
         initialView="dayGridMonth"
         buttonText={{
-          today: "오늘",
-          month: "월",
-          week: "주",
-          day: "일",
-          list: "목록",
+          today: '오늘',
+          month: '월',
+          week: '주',
+          day: '일',
+          list: '목록',
         }}
         nowIndicator
         titleFormat="yyyy.{MM}"
@@ -65,7 +70,13 @@ const Calendar = () => {
         dayMaxEventRows={6}
         slotDuration="00:30:00"
         slotLabelInterval="01:00"
-        events={[]}
+        events={[
+          {
+            title: 'HELLO',
+            start: '2020-10-01 00:00:00',
+            end: '2020-10-03 15:00:00',
+          },
+        ]}
         select={handleDateSelect}
         eventContent={renderEventContent}
         eventClick={handleEventClick}
