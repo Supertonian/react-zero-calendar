ea7e1020213f1c99f2d61b4d777420fa34ed0f1c
wwwaaa <python.signal@gmail.com>
2020-10-18 22:36:05 +0900

    Updates

--- precache-manifest.1f535c632cb8ee2fd128abcb14c1c59c.js
+++ precache-manifest.1f535c632cb8ee2fd128abcb14c1c59c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4aa5e21577a2f77081f47f2af997dcc",
+    "revision": "fca2fa625e438401f81db271d2e2624f",
     "url": "/calendar/index.html"
   },
   {
-    "revision": "93c18cf905ee934e58c7",
+    "revision": "ab962b17b64802eaf879",
     "url": "/calendar/static/css/2.b145d9db.chunk.css"
   },
   {
-    "revision": "84acbaaf8a48e557c1d1",
+    "revision": "31e9408596864a89f29b",
     "url": "/calendar/static/css/main.69eda509.chunk.css"
   },
   {
-    "revision": "93c18cf905ee934e58c7",
-    "url": "/calendar/static/js/2.955e22d8.chunk.js"
+    "revision": "ab962b17b64802eaf879",
+    "url": "/calendar/static/js/2.ae4a2590.chunk.js"
   },
   {
     "revision": "b14d10d0a85a9c147d7d2e44e7de51c5",
-    "url": "/calendar/static/js/2.955e22d8.chunk.js.LICENSE.txt"
+    "url": "/calendar/static/js/2.ae4a2590.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "84acbaaf8a48e557c1d1",
-    "url": "/calendar/static/js/main.33f2bfa8.chunk.js"
+    "revision": "31e9408596864a89f29b",
+    "url": "/calendar/static/js/main.cf8c4e21.chunk.js"
   },
   {
     "revision": "7d0fb5c2975c2066ee7b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calendar/precache-manifest.3f8a234ee7e8d35cc9dccda6b477425d.js"
+  "/calendar/precache-manifest.1f535c632cb8ee2fd128abcb14c1c59c.js"
 );
 
 self.addEventListener('message', (event) => {
