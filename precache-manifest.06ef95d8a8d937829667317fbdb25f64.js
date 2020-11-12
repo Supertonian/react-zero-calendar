d03125ef016b5fbf0d81bc9e52e55770fcc24e36
wwwaaa <python.signal@gmail.com>
2020-11-12 11:34:02 +0900

    Updates

---
+++ precache-manifest.06ef95d8a8d937829667317fbdb25f64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1640ede212021d19db9d06a4e28626a9",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "d21c145a4d9c76040618",
+    "url": "/calendar/static/css/2.1ac2534d.chunk.css"
+  },
+  {
+    "revision": "737ab4c22594fb09c950",
+    "url": "/calendar/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d21c145a4d9c76040618",
+    "url": "/calendar/static/js/2.6d493a44.chunk.js"
+  },
+  {
+    "revision": "83955c0e10f0a1093e43eb44b6b2b037",
+    "url": "/calendar/static/js/2.6d493a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737ab4c22594fb09c950",
+    "url": "/calendar/static/js/main.ae6ea8fd.chunk.js"
+  },
+  {
+    "revision": "7d0fb5c2975c2066ee7b",
+    "url": "/calendar/static/js/runtime-main.914f0415.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calendar/precache-manifest.adaa6579ca78ae894f31681543cf41b0.js"
+  "/calendar/precache-manifest.06ef95d8a8d937829667317fbdb25f64.js"
 );
 
 self.addEventListener('message', (event) => {
