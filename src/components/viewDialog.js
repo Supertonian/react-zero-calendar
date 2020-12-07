4ffd9cb2531dcc597660ec43dd19cbcbb2975b44
minjikim <minji_kim2@tmax.co.kr>
2020-12-07 11:19:09 +0900

    feat(edit): edit 기능 flag로 수정 다이얼로그 삭제

--- src/components/viewDialog.js
+++ src/components/viewDialog.js
@@ -4,48 +4,119 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
+import { Container, DialogContentText, Grid, Input, FormControlLabel, Checkbox, Button } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import { makeStyles } from '@material-ui/core/styles';
-import { EditDialog } from './editDialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import { MuiPickersUtilsProvider, DateTimePicker } from '@material-ui/pickers';
+import LuxonUtils from '@date-io/luxon';
+import { ColorPalette } from 'material-ui-color';
+import { DateTime } from 'luxon';
+
 const ViewDialogComponent = ({ setOpen, event, deleteEvent, editEvent }) => {
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('xs'));
-  const [editDialogOpen, setEditDialogOpen] = React.useState(false);
+  const [title, setTitle] = React.useState();
+  const [selectedStartDate, setSelectedStartDate] = React.useState();
+  const [selectedEndDate, setSelectedEndDate] = React.useState();
+  const [allDay, setAllDay] = React.useState(false);
+  const [important, setImportant] = React.useState();
+  const [place, setPlace] = React.useState();
+  const [color, setColor] = React.useState('black');
+  const [editFlag, setEditFlag] = React.useState(false);
+
+  const palette = {
+    IndianRed: '#CD5C5C',
+    Crimson: '#DC143C',
+    Tomato: '#FF6347',
+    Gold: '#FFD700',
+    Magenta: '#FF00FF',
+    RebeccaPurple: '#663399',
+    Indigo: '#4B0082',
+    Lime: '#00FF00',
+    ForestGreen: '#228B22',
+    SaddleBrown: '#8B4513',
+  };
+
+  function handleSave() {
+    const isAllDay = (selectedEndDate - selectedStartDate) / (60 * 60 * 1000) >= 24;
+
+    const editInfo = {
+      type: 'list',
+      id: event.id,
+      title: event.title,
+      start: event.startStr,
+      end: event.endStr,
+      allDay: event.allDay,
+      display: event.display,
+      place: event.extendedProps.place,
+      forceAllDay: event.forceAllDay,
+      color: event.color,
+    };
+
+    const eventInfo = {
+      start: selectedStartDate.toISO(),
+      end: selectedEndDate.toISO(),
+      important,
+      title:
+        title.trim() === ''
+          ? '(제목없음)'
+          : important === true && !title.includes('(중요')
+          ? title.concat('(중요)')
+          : title,
+      allDay: allDay || isAllDay,
+      place,
+      forceAllDay: allDay,
+      color,
+    };
+    editEvent(editInfo.id, eventInfo);
+    setOpen(false);
+  }
+
   function handleClose() {
     setOpen(false);
   }
+  const handleTitle = event => {
+    setTitle(event.target.value);
+  };
+
+  const handleAllDayChange = event => {
+    setAllDay(event.target.checked);
+  };
+
+  const handleImportantChange = event => {
+    setImportant(event.target.checked);
+  };
+  const handlePlaceChange = event => {
+    setPlace(event.target.value);
+  };
+  const handleStartDateChange = date => {
+    setSelectedStartDate(date);
+  };
+
+  const handleEndDateChange = date => {
+    setSelectedEndDate(date);
+  };
+
   const useStyles = makeStyles(theme => ({
     marginIcon: {
       marginLeft: 200,
     },
   }));
-
-  const editInfo = {
-    type: 'list',
-    id: event.id,
-    title: event.title,
-    start: event.startStr,
-    end: event.endStr,
-    allDay: event.allDay,
-    display: event.display,
-    place: event.extendedProps.place,
-    forceAllDay: event.forceAllDay,
-    color: event.color,
-  };
   const classes = useStyles();
+
+  function nvl(str, defaultStr) {
+    if (typeof str === 'undefined' || str === null || str === '') str = '';
+    return str;
+  }
+
   return (
     <>
-      <Dialog
-        fullScreen={fullScreen}
-        open={true}
-        onClose={handleClose}
-        aria-labelledby="responsive-view-dialog"
-      >
+      <Dialog fullScreen={fullScreen} open={true} onClose={handleClose} aria-labelledby="responsive-view-dialog">
         <DialogTitle id="responsive-dialog-title" style={{ textAlign: 'left' }}>
           {' '}
           일정
@@ -53,8 +124,13 @@ const ViewDialogComponent = ({ setOpen, event, deleteEvent, editEvent }) => {
             className={classes.marginIcon}
             aria-label="edit-button"
             onClick={() => {
-              setEditDialogOpen(true);
-              //handleClose();
+              setEditFlag(true);
+              setTitle(nvl(event.title));
+              setPlace(nvl(event.extendedProps?.place));
+              setSelectedStartDate(DateTime.fromISO(event.startStr));
+              setSelectedEndDate(DateTime.fromISO(event.endStr));
+              setAllDay(event.allDay);
+              setImportant(event.important);
             }}
           >
             <EditOutlinedIcon />
@@ -75,17 +151,73 @@ const ViewDialogComponent = ({ setOpen, event, deleteEvent, editEvent }) => {
             <CloseIcon />
           </IconButton>
         </DialogTitle>
-        <DialogContent style={{ height: '200px' }} dividers>
-          <Container>{event.title}</Container>
-          <Container>{`${event.startStr} - ${event.endStr}`}</Container>
-          <Container>{event.importance}</Container>
-          <Container>{event.allDay}</Container>
-          <Container>{event.extendedProps?.place}</Container>
-        </DialogContent>
+        {editFlag && (
+          <>
+            <DialogContent>
+              <DialogContentText>
+                <Container>
+                  <Grid container>
+                    <Input autoFocus placeholder="제목을 입력해주세요" value={title} onChangeCapture={handleTitle} />
+                    <FormControlLabel
+                      control={<Checkbox checked={allDay} onChange={handleAllDayChange} name="allDay" />}
+                      label="종일"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox checked={important} onChange={handleImportantChange} name="important" />}
+                      label="중요"
+                    />
+                  </Grid>
+                </Container>
+                <Container>
+                  <MuiPickersUtilsProvider utils={LuxonUtils}>
+                    <Grid container justify="space-around">
+                      <DateTimePicker
+                        margin="normal"
+                        id="edit-datetime-picker"
+                        label="시작 날짜 시간"
+                        format="yyyy-MM-dd HH:mm:ss"
+                        value={selectedStartDate}
+                        onChange={handleStartDateChange}
+                      />
+                      <DateTimePicker
+                        margin="normal"
+                        id="end-datetime-picker"
+                        label="끝 날짜 시간"
+                        format="yyyy-MM-dd HH:mm:ss"
+                        value={selectedEndDate}
+                        onChange={handleEndDateChange}
+                      />
+                    </Grid>
+                  </MuiPickersUtilsProvider>
+                </Container>
+                <Container>
+                  <Input placeholder="장소를 입력해주세요" value={place} onChange={handlePlaceChange} />
+                </Container>
+                <Container>
+                  <ColorPalette palette={palette} value={color} onSelect={setColor} />
+                </Container>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="secondary">
+                취소
+              </Button>
+              <Button onClick={handleSave} color="primary">
+                저장
+              </Button>
+            </DialogActions>
+          </>
+        )}
+        {editFlag === false && (
+          <DialogContent style={{ height: '200px' }} dividers>
+            <Container>{event.title}</Container>
+            <Container>{`${event.startStr} - ${event.endStr}`}</Container>
+            <Container>{event.importance}</Container>
+            <Container>{event.allDay}</Container>
+            <Container>{event.extendedProps?.place}</Container>
+          </DialogContent>
+        )}
       </Dialog>
-      {editDialogOpen && (
-        <EditDialog setOpen={setEditDialogOpen} editEvent={editEvent} editInfo={editInfo} />
-      )}
     </>
   );
 };
